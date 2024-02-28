8a062395a22f356dc1c09020a3bbd98bfe3c8dc1
mavedian <martin.avedian@hotmail.fr>
2024-02-28 15:33:50 +0100

    modification submit

--- api/submit.js
+++ api/submit.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   // Votre logique ici...
   const { interests, businessInfo, personalInfo } = req.body;
   console.log(req.body);
-  res.status(200).json({ message: 'Réponse de la fonction serverless' });
+  //res.status(200).json({ message: 'Réponse de la fonction serverless' });
     
     // Préparez les données pour l'envoi à HubSpot
     const hubspotData = {
@@ -30,7 +30,7 @@ module.exports = async (req, res) => {
     };
 
     try {
-        const response = await axios.post(`https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formId}`, hubspotData, {
+        await axios.post(`https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formId}`, hubspotData, {
             headers: {
                 'Content-Type': 'application/json'
             }

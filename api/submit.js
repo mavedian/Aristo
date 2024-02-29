8bd1e4b4808db2eca77fa16f76785eb67907d02b
mavedian <martin.avedian@hotmail.fr>
2024-02-29 09:52:42 +0100

    modification submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,12 +1,39 @@
 // api/submit.js
-const axios = require('axios');
+//const axios = require('axios');
 const portalId = "143937373";
 const formId = "a7065899-7249-4a94-933f-4aa64a0b0349";
+
+require('dotenv').config();
+//console.log(process.env)
+const express = require('express');
+const cors = require('cors');
+const Airtable = require('airtable');
+const app = express();
+const axios = require('axios');
+const path = require('path');
+
+
+// Référencer le dossier où se trouvent vos fichiers statiques, comme index.html
+app.use(express.static(path.join(__dirname, 'public')));
+// Middleware pour parser le corps des requêtes en JSON
+app.use(cors({
+    // Définissez les origines que vous souhaitez autoriser
+    origin: '*', // Ou une liste spécifique d'origines 'https://mondomaine.com'
+}));
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "frame-ancestors 'self' https://framer.com https://framerusercontent.com https://*.framer.com https://*.framerusercontent.com;");
+    next();
+  });
+
+
 
 module.exports = async (req, res) => {
   // Votre logique ici...
   const { interests, businessInfo, personalInfo } = req.body;
-  
+  console.log(req.body);
+  console.log(portalId);
+  console.log(formId);  
   //res.status(200).json({ message: 'Réponse de la fonction serverless' });
     
     // Préparez les données pour l'envoi à HubSpot
@@ -45,3 +72,9 @@ module.exports = async (req, res) => {
         res.status(500).json({ error: "Erreur lors de l'envoi des données à HubSpot." });
     }
 };
+
+// Définissez le port pour votre serveur
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+});
